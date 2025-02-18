29c5260c29662dda4eff4dd3b5b198f69fcd9d96
thicced-witch <thiccedwitch@gmail.com>
2025-02-17 19:11:08 -0500

    Add files via upload

---
+++ js/archive.js
@@ -0,0 +1,25 @@
+const archiveText = [
+    {
+        name: 'What is Trans/Misogyny',
+        body: `<h1>What Is Trans/Misogyny</h1>
+        <p>
+        Trans/Misogyny has two heads. The first head is what we recognize as the intersection of transphobia and misogyny as experienced by trans women, a term first coined by Julia Serano in her 2007 book Whipping Girl. 
+The second head is the societal force, Trans/Misogyny. This force creates what we recognize as Gendered Reality, and it's within this reality that all experiences with gender occur.<br/><br/>
+Like its sister, Trans/Misogyny is the combined force of transphobia and misogyny.  Misogyny creates the Gender Binary, which is the belief in the imaginary concepts we call Men and Women. In reality, what we recognize as Men and Women are nothing more than shorthand, meant to refer to an arbitrary collection of physical and behavioral traits. Yet due to the effects of misogyny, it’s widely believed that Men and Women are fundamental concepts with as much basis in reality as gravity.
+<br/><br/>Through misogyny, the Gender Binary is created, and through transphobia, its existence is maintained. Transphobia stems from the belief that the imaginary concepts of Men and Women are mutually exclusive, and as such, movement from one class to the other is impossible. Transphobia leads its believers to violently police these classes, ensuring that no individual attempts movement from one class to the other.<br/><br/>
+These twin forces combine to create Trans/Misogyny, and the force of Trans/Misogyny acting on Objective Reality creates what we recognize as Gendered Reality.
+In this space, not only are Men and Women “real”, they have always been real, and will always be real. As such, the logic of Gendered Reality will always affirm our belief in Trans/Misogyny, looping in on itself in service of proving one equation TRUE. The only equation that will always, with 100% certainty, be TRUE within Gendered Reality:
+<h1>TRANS/MISOGYNY = TRUE</h1>
+        </p>`
+    },
+    {
+        name: 'What is Trans/Misogynistic-Bias',
+        body: `<h1>What Is <br/>Trans/Misogynistic-Bias</h1>
+        <p>
+        Trans/Misogynistic-Bias is the function through which Trans/Misogyny perpetuates itself. Biases are cognitive loops, ones created by contradictions within an individual's logic. These loops lead the biased individual towards conclusions which prove the contradiction TRUE, even when it is objectively FALSE. As its name suggests, Trans/Misogynistic-Bias is a cognitive loop created by the contradiction that is Trans/Misogyny.<br/><br/>
+The Trans/Misogynistic-Bias loop leads the biased individual towards conclusions which assert that Women and Men are real, as logic which asserts that Women and Men are real will inherently prove that the Gender Binary is real. Since the Gender Binary cannot be real without Trans/Misogyny to sustain it, its existence inherently proves that Trans/Misogyny is real, which then goes on to prove the Gender Binary real.<br/><br/> Trans/Misogyny proves the Gender Binary, which proves Trans/Misogyny in turn. 
+This feedback loop is what gives birth to Gendered Reality, and due to this reality's structure being built on trans/misogynistic logic, logic within Gendered Reality will always affirm Trans/Misogynistic-Bias. 
+<br/><br/>It's because of this that Trans/Misogyny = TRUE is a universal constant within Gendered Reality, though the simplicity of this equation belies the true nature of Trans/Misogynistic-Bias. Because just as Trans/Misogyny is composed of both transphobia and misogyny, Trans/Misogynistic-Bias is given shape by twin cognitive loops.
+        </p>`
+    },
+]
---
+++ js/archiveButtons.js
@@ -0,0 +1,21 @@
+let textBox = document.querySelector('.textBox')
+let archiveBox = document.querySelector('.archive')
+
+
+// Create Buttons for other textBoxes
+
+for (let archive of archiveText) {
+    let button = document.createElement('button')
+    button.classList.add('button')
+    button.innerText = `${archive.name}`
+    archiveBox.appendChild(button)
+
+    // Adds Click Function to buttons that replaces text
+
+    button.addEventListener("click", function (){
+        if (textBox.innerText != null) {
+            textBox.innerText = null;
+        }
+        textBox.innerHTML = `${archive.body}`
+    })
+}
