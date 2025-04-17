a8caa7cfec5c9192c1d7f2c16f6f12140d494eb3
thicced-witch <thiccedwitch@gmail.com>
2025-04-17 15:52:09 -0400

    Add files via upload

--- js/archive.js
+++ js/archive.js
@@ -22,4 +22,15 @@ This feedback loop is what gives birth to Gendered Reality, and due to this real
 <br/><br/>It's because of this that Trans/Misogyny = TRUE is a universal constant within Gendered Reality, though the simplicity of this equation belies the true nature of Trans/Misogynistic-Bias. Because just as Trans/Misogyny is composed of both transphobia and misogyny, Trans/Misogynistic-Bias is given shape by twin cognitive loops.
         </p>`
     },
+    {
+    name: 'Passive Bias vs. Active Bias',
+    body: `<h1>Passive Bias vs. <br/>Active Bias</h1>
+    <p>
+    The <b>passive trans/misogynist</b> is someone unaware of their Trans/Misogynistic-Bias.<br/> This occurs when an individual is born into Gendered Reality and becomes normalized to its trans/misogynistic logic. <br/><br/>Due to their lifetime of social conditioning, the passive trans/misogynist automatically reaches conclusions which affirm their <br/>Trans/Misogynistic-Bias.
+    <br/><br/>Passive Trans/Misogynists are unaware of this process, but they still possess the ability to recognize trans/misogynistic beliefs, and may even view Trans/Misogyny negatively.<br/><br/> These individuals can memorize conclusions which others have identified as trans/misogynistic, yet they lack the experience necessary to identify the trans/misogynistic thought patterns which lead to these conclusions.
+    <br/><br/>The moment a passive trans/misogynist becomes aware of their Trans/Misogynistic-Bias, they cease being passive and become active trans/misogynists.<br/><br/>
+The <b>active trans/misogynist</b> is someone aware of their Trans/Misogynistic-Bias. This awareness allows them to engage directly with their bias, identifying beliefs or behavior which are <br/>trans/misogynistic in nature.<br/><br/> While these individuals are aware of their bias, they do not necessarily view the bias negatively. Often it’s the case that the active trans/misogynist finds themself biased in favor of Trans/Misogyny, and recognize trans/misogynistic logic as Objective Logic. 
+<br/><br/>Regardless of whether passive or active, <br/>Trans/Misogynistic-Bias will always reach towards the same conclusion;  Affirmation of gender as real, and perpetuation of the class system created by the gender binary.
+    </p>`
+},
 ]
